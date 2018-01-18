3b21563fd4e39379da3bf8e0b63f2f0e1e53bb6e
Sponsology <sponsology1@gmail.com>
2018-01-17 18:32:13 -0800

    Updated privacy policy text

--- src/inputTypes/privacyInput.js
+++ src/inputTypes/privacyInput.js
@@ -48,10 +48,11 @@ class PrivacyInput extends React.Component {
                  onBlur={this.props.onBlur.bind(null, (this.state.checked
                                                         ? this.props.value
                                                         : undefined))} />
-          By checking this box, I agree to the terms of the&nbsp;
+          <p>Welcome. You are about to set up your Account and Profile with our community.  Your Profile will be comprehensive to optimise your matches.  You can save this form as you go.</p>
+          <p>By checking this box, I agree to the terms of the&nbsp;
           <a href="/privacy-policy">Privacy Policy</a>
           , and the&nbsp;
-          <a href="/terms">Terms and Conditions</a>
+          <a href="/terms">Terms and Conditions</a></p>
 
         </label>
       </div>
