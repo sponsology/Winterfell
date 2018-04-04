4f469f8802d23e0c7ef8a9a3115853c0e0f0123d
Sponsology <sponsology1@gmail.com>
2018-04-03 23:02:06 -0700

    Returned to original button

--- dist/button.js
+++ dist/button.js
@@ -4,49 +4,35 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var _reactProgressButton = require('react-progress-button');
-
-var _reactProgressButton2 = _interopRequireDefault(_reactProgressButton);
 
 var React = require('react');
 
 var Button = (function (_React$Component) {
   _inherits(Button, _React$Component);
 
-  function Button(props) {
+  function Button() {
     _classCallCheck(this, Button);
 
-    _get(Object.getPrototypeOf(Button.prototype), 'constructor', this).call(this, props);
-    this.state = { buttonState: props.buttonState };
+    _get(Object.getPrototypeOf(Button.prototype), 'constructor', this).apply(this, arguments);
   }
 
   _createClass(Button, [{
     key: 'handleClick',
     value: function handleClick(e) {
       e.preventDefault();
+
       this.props.onClick();
-    }
-  }, {
-    key: 'componentWillReceiveProps',
-    value: function componentWillReceiveProps(nextProps) {
-      this.setState({
-        buttonState: nextProps.buttonState
-      });
     }
   }, {
     key: 'render',
     value: function render() {
       return React.createElement(
-        _reactProgressButton2['default'],
+        'button',
         { href: '#',
-          controlled: true,
-          state: this.state.buttonState,
+          className: this.props.className,
           onClick: this.handleClick.bind(this) },
         this.props.text
       );
@@ -59,7 +45,6 @@ var Button = (function (_React$Component) {
 ;
 
 Button.defaultProps = {
-  buttonState: '',
   text: 'Submit',
   className: undefined,
   onClick: function onClick() {}
--- src/button.js
+++ src/button.js
@@ -1,38 +1,26 @@
 var React = require('react');
-import ProgressButton from 'react-progress-button'
 
 class Button extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {buttonState: props.buttonState}
-  }
 
   handleClick(e) {
     e.preventDefault();
-    this.props.onClick();
-  }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      buttonState     : nextProps.buttonState
-    });
+    this.props.onClick();
   }
 
   render() {
     return (
-      <ProgressButton href="#"
-         controlled={true}
-         state={this.state.buttonState}
+      <button href="#"
+         className={this.props.className}
          onClick={this.handleClick.bind(this)}>
         {this.props.text}
-      </ProgressButton>
+      </button>
     );
   }
 
 };
 
 Button.defaultProps = {
-  buttonState: '',
   text      : 'Submit',
   className : undefined,
   onClick   : () => {}
