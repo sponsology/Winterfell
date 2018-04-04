604152a286af0f8badd7560a87dd29d82c66ba28
Sponsology <sponsology1@gmail.com>
2018-04-04 00:04:12 -0700

    Yet more logging

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -41,6 +41,8 @@ var validateAnswer = (value, validationItem, questionAnswers) => {
                                ? Validator[validationItem.type]
                                : undefined;
 
+  console.log("Validating " + value + " agianst " + validationItem)
+
   if (!validationMethod) {
     throw new Error('Winterfell: Attempted to validate for undefined method "'
                     + validationItem.type + '"');
@@ -73,7 +75,9 @@ var validateAnswer = (value, validationItem, questionAnswers) => {
    * Return the result of the validation method running
    * wtih the validationParameters.
    */
+   console.log("Returning ", validationMethod.apply(null, validationParameters))
   return validationMethod.apply(null, validationParameters);
+
 };
 
 /**
@@ -151,7 +155,7 @@ var getQuestionPanelInvalidQuestions = (questionSets, questionAnswers) => {
                              return question.validations instanceof Array
                                       && question.validations.length > 0;
                            });
-
+  console.log("questions to check ", questionsToCheck)
   /*
    * Now we run validations for the questions
    * we need to check for errors.
