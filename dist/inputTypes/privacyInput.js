320b29d2162bcbe16dfe0370a6345d2604af5f3c
sponsology <sponsology1@gmail.com>
2018-11-28 22:00:58 -0800

    Updated privacy input for optional text

--- dist/inputTypes/privacyInput.js
+++ dist/inputTypes/privacyInput.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var React = require('react');
+var React = require("react");
 
 var PrivacyInput = (function (_React$Component) {
   _inherits(PrivacyInput, _React$Component);
@@ -16,7 +16,7 @@ var PrivacyInput = (function (_React$Component) {
   function PrivacyInput(props) {
     _classCallCheck(this, PrivacyInput);
 
-    _get(Object.getPrototypeOf(PrivacyInput.prototype), 'constructor', this).call(this, props);
+    _get(Object.getPrototypeOf(PrivacyInput.prototype), "constructor", this).call(this, props);
 
     this.state = {
       checked: props.defaultChecked
@@ -24,7 +24,7 @@ var PrivacyInput = (function (_React$Component) {
   }
 
   _createClass(PrivacyInput, [{
-    key: 'handleChange',
+    key: "handleChange",
     value: function handleChange(e) {
       var _this = this;
 
@@ -39,50 +39,58 @@ var PrivacyInput = (function (_React$Component) {
       }
     }
   }, {
-    key: 'componentDidMount',
+    key: "componentDidMount",
     value: function componentDidMount() {
       if (this.state.checked) {
         this.handleChange();
       }
     }
   }, {
-    key: 'render',
+    key: "render",
     value: function render() {
       return React.createElement(
-        'div',
+        "div",
         { className: this.props.classes.checkboxListItem },
-        React.createElement(
-          'p',
+        this.props.seeker ? React.createElement(
+          "p",
           null,
-          'You have set up your Account details.  Now you will step through setting up your Profile to find sponsorship alignment opportunities with Givers.  Next step is payment of your annual subscription.'
+          "You have set up your Account details. Now you will step through setting up your Profile to find sponsorship alignment opportunities with Givers. Next step is payment of your annual subscription."
+        ) : React.createElement(
+          "p",
+          null,
+          "You have set up your Account details. Now you will step through setting up your Profile to find sponsorship alignment opportunities with Seekers."
         ),
-        React.createElement('input', { type: 'checkbox',
+        React.createElement("input", {
+          type: "checkbox",
           name: this.props.name,
-          id: 'id-privacy',
+          id: "id-privacy",
           autoComplete: this.props.autoComplete,
-          'aria-labelledby': this.props.labelId,
+          "aria-labelledby": this.props.labelId,
           className: this.props.classes.checkbox,
           defaultChecked: this.state.checked,
           value: this.props.value,
-          required: 'required',
+          required: "required",
           onChange: this.handleChange.bind(this),
-          onBlur: this.props.onBlur.bind(null, this.state.checked ? this.props.value : undefined) }),
+          onBlur: this.props.onBlur.bind(null, this.state.checked ? this.props.value : undefined)
+        }),
         React.createElement(
-          'label',
-          { className: this.props.classes.checkboxLabel,
+          "label",
+          {
+            className: this.props.classes.checkboxLabel,
             id: this.props.labelId,
-            htmlFor: 'id-privacy' },
-          'by checking this box, I agree to the terms of the ',
+            htmlFor: "id-privacy"
+          },
+          "by checking this box, I agree to the terms of the ",
           React.createElement(
-            'a',
-            { href: '/privacy-policy' },
-            'Privacy Policy'
+            "a",
+            { href: "/privacy-policy" },
+            "Privacy Policy"
           ),
-          ', and the ',
+          ", and the ",
           React.createElement(
-            'a',
-            { href: '/terms' },
-            'Terms and Conditions'
+            "a",
+            { href: "/terms" },
+            "Terms and Conditions"
           )
         )
       );
@@ -92,15 +100,14 @@ var PrivacyInput = (function (_React$Component) {
   return PrivacyInput;
 })(React.Component);
 
-;
-
 PrivacyInput.defaultProps = {
-  text: '',
+  text: "",
   defaultChecked: false,
   classes: {},
-  name: '',
-  autoComplete: '',
-  value: '',
+  name: "",
+  autoComplete: "",
+  value: "",
+  seeker: true,
   onChange: function onChange() {},
   onBlur: function onBlur() {}
 };
--- src/inputTypes/privacyInput.js
+++ src/inputTypes/privacyInput.js
@@ -1,28 +1,28 @@
-var React = require('react');
+var React = require("react");
 
 class PrivacyInput extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      checked : props.defaultChecked
+      checked: props.defaultChecked
     };
   }
 
   handleChange(e) {
     if (e) {
-      this.setState({
-        checked : !this.state.checked
-      }, () => {
-        this.props.onChange(this.state.checked
-                            ? this.props.value
-                            : undefined);
-      });
+      this.setState(
+        {
+          checked: !this.state.checked
+        },
+        () => {
+          this.props.onChange(
+            this.state.checked ? this.props.value : undefined
+          );
+        }
+      );
     } else {
-      this.props.onChange(this.state.checked
-                            ? this.props.value
-                            : undefined);
+      this.props.onChange(this.state.checked ? this.props.value : undefined);
     }
   }
 
@@ -35,45 +35,60 @@ class PrivacyInput extends React.Component {
   render() {
     return (
       <div className={this.props.classes.checkboxListItem}>
-        <p>You have set up your Account details.  Now you will step through setting up your Profile to find sponsorship alignment opportunities with Givers.  Next step is payment of your annual subscription.</p>
-          <input type="checkbox"
-                 name={this.props.name}
-                 id='id-privacy'
-                 autoComplete={this.props.autoComplete}
-                 aria-labelledby={this.props.labelId}
-                 className={this.props.classes.checkbox}
-                 defaultChecked={this.state.checked}
-                 value={this.props.value}
-                 required='required'
-                 onChange={this.handleChange.bind(this)}
-                 onBlur={this.props.onBlur.bind(null, (this.state.checked
-                                                        ? this.props.value
-                                                        : undefined))} />
-          <label className={this.props.classes.checkboxLabel}
-               id={this.props.labelId}
-               htmlFor='id-privacy'>
+        {this.props.seeker ? (
+          <p>
+            You have set up your Account details. Now you will step through
+            setting up your Profile to find sponsorship alignment opportunities
+            with Givers. Next step is payment of your annual subscription.
+          </p>
+        ) : (
+          <p>
+            You have set up your Account details. Now you will step through
+            setting up your Profile to find sponsorship alignment opportunities
+            with Seekers.
+          </p>
+        )}
 
+        <input
+          type="checkbox"
+          name={this.props.name}
+          id="id-privacy"
+          autoComplete={this.props.autoComplete}
+          aria-labelledby={this.props.labelId}
+          className={this.props.classes.checkbox}
+          defaultChecked={this.state.checked}
+          value={this.props.value}
+          required="required"
+          onChange={this.handleChange.bind(this)}
+          onBlur={this.props.onBlur.bind(
+            null,
+            this.state.checked ? this.props.value : undefined
+          )}
+        />
+        <label
+          className={this.props.classes.checkboxLabel}
+          id={this.props.labelId}
+          htmlFor="id-privacy"
+        >
           by checking this box, I agree to the terms of the&nbsp;
-          <a href="/privacy-policy">Privacy Policy</a>
-          , and the&nbsp;
+          <a href="/privacy-policy">Privacy Policy</a>, and the&nbsp;
           <a href="/terms">Terms and Conditions</a>
-
         </label>
       </div>
     );
   }
-
-};
+}
 
 PrivacyInput.defaultProps = {
-  text     : '',
+  text: "",
   defaultChecked: false,
-  classes  : {},
-  name     : '',
-  autoComplete: '',
-  value    : '',
-  onChange : () => {},
-  onBlur   : () => {}
+  classes: {},
+  name: "",
+  autoComplete: "",
+  value: "",
+  seeker: true,
+  onChange: () => {},
+  onBlur: () => {}
 };
 
 module.exports = PrivacyInput;
